6655bce86c13f50f87dc01eccb356c30e52a1aa9
andite17 <andi.hanafi.te17@mhsw.pnj.ac.id>
2022-01-10 19:22:36 +0700

    adding navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,90 @@
+import React from 'react'
+import { Badge } from '@material-ui/core'
+import { Search, ShoppingCartOutlined } from '@material-ui/icons'
+// import styles from '../pages/styledComponent/Home.module.css'
+import styled from '@emotion/styled'
+
+const Container = styled.div`
+    height: 60px;
+`
+
+const Wraper = styled.div`
+    padding: 10px 20px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
+
+const Left = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: center;
+`
+const Lang = styled.span`
+    font-size: 14px;
+    cursor: pointer
+`
+const SearchContainer = styled.div`
+    border: .5px solid lightgray;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    padding: 5px;
+`
+const Input = styled.input`
+    border: none;
+`
+const Center = styled.div`
+    flex:1;
+    align-items: center;
+    text-align: center;
+`
+
+const Logo = styled.h1`
+    font-weight: bold;
+`
+
+const Right = styled.div`
+    flex:1;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end
+`
+const MenuItem = styled.div`
+    font-size: 14px;
+    cursor: pointer;
+    margin-left: 2rem;
+` 
+    
+    function Navbar() {
+        return (
+            <Container>
+            <Wraper>
+                <Left>
+                    <Lang>EN</Lang>
+                    <SearchContainer>
+                        <Input/>
+                        <Search style={{color:"gray", fontSize:16}} />
+                    </SearchContainer>
+                </Left>
+                <Center>
+                    <Logo>
+                        ELEC
+                    </Logo>
+                    
+                </Center>
+                <Right>
+                    <MenuItem>REGISTER</MenuItem>
+                    <MenuItem>SIGN IN</MenuItem>
+                    <MenuItem>
+                        <Badge badgeContent={4} color="primary">
+                            <ShoppingCartOutlined />
+                        </Badge>
+                    </MenuItem>
+                </Right>
+            </Wraper>
+        </Container>
+    )
+}
+
+export default Navbar
