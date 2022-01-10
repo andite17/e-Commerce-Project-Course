9a98b7da41acb46c10d597da886f008467da9620
andite17 <andi.hanafi.te17@mhsw.pnj.ac.id>
2022-01-11 00:45:41 +0700

    add category component

---
+++ src/components/Categories.jsx
@@ -0,0 +1,21 @@
+import styled from "@emotion/styled"
+import { categories } from '../data'
+import CategoryItem from './CategoryItem'
+
+const Container = styled.div`
+    display: flex;
+    padding: 20px;
+`
+
+const Categories = () => {
+    return (
+        <Container>
+            {categories.map(item => (
+                <CategoryItem item={item} />
+            ))}
+        </Container>
+    )
+}
+
+export default Categories
+
---
+++ src/components/Product.jsx
@@ -0,0 +1,16 @@
+import { styled } from "@material-ui/core"
+import { popularProducts } from '../data'
+
+const Container = styled.div`
+    
+`
+
+const Product = () => {
+    return (
+        <Container>
+            
+        </Container>
+    )
+}
+
+export default Product
